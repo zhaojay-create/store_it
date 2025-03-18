e5760c3f1a043f0e4c6592299a3b37a2293b45ff
赵杰 <zhaojay1211@gmail.com>
2025-03-19 01:10:28 +0800

    feat: finish tailwindcss config a little

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div className="text-3xl">StoreIt - The only storage solution you need</div>
+    <div className="center h-screen bg-amber-100">
+      <div className="text-3xl font-bold text-brand-100">
+        StoreIt - The only storage solution you need
+      </div>
+    </div>
   );
 }

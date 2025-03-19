f60bf258aa38431f3bdff5403127d78eeb7d73ae
赵杰 <zhaojay1211@gmail.com>
2025-03-19 23:03:30 +0800

    feat: 完成登录注册页面

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex flex-col items-center justify-center max-h-[800px] max-w-[430px] space-y-2">
+          <Image
+            src="/assets/logo.png"
+            alt="logo"
+            width={56}
+            height={56}
+            className="h-auto"
+          />
+          <div className="space-y-5 text-white">
+            <h1 className="text-3xl font-bold">
+              Manane your files the best way
+            </h1>
+            <p className="text-lg">
+              this is a place where you can store your files for free
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-1 flex-col items-center justify-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">a image will be here</div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default layout;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { z } from "zod";
+import React, { FC, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+type FormType = "sign-up" | "sign-in";
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
+interface AuthFormProps {
+  type: FormType;
+}
+
+const AuthForm: FC<AuthFormProps> = ({ type }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <h1 className="text-2xl text-center font-semibold">
+            {type === "sign-up" ? "Sign Up" : "Sign In"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem className="flex flex-col w-full">
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="w-full"
+                      placeholder="enter your full name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="flex flex-col w-full">
+                <FormLabel>email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full"
+                    placeholder="enter your email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isLoading}>
+            {type === "sign-up" ? "Sign Up" : "Sign In"}
+            {isLoading && "loading..."}
+          </Button>
+          {errorMessage && (
+            <p className="text-red-500 text-center">{errorMessage}</p>
+          )}
+
+          <div className="flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-in"
+                ? "Don't have an account ?"
+                : "Already have an account ?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="text-brand-100"
+            >
+              {type === "sign-in" ? "sign-up" : "sign-in"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+
+      {/* OTP verify */}
+    </>
+  );
+};
+
+export default AuthForm;

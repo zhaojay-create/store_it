b79bf0ac619b0de1f3fb0e158923f9363d2f9731
赵杰 <zhaojay1211@gmail.com>
2025-04-03 00:38:49 +0800

    feat: 完成弹出的逻辑

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import React, { FC, useState } from "react";
-import { Dialog } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,6 +23,8 @@ import { actionsDropdownItems } from "@/constants";
 import { ActionType } from "@/types";
 import { constructDownloadUrl } from "@/lib/utils";
 import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 interface ActionDropdownProps {
   file: Models.Document;
@@ -24,6 +34,61 @@ const ActionDropdown: FC<ActionDropdownProps> = ({ file }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropdownOpen(false);
+    setAction(null);
+    setName(file.name);
+    // setEmails([]);
+  };
+
+  const handleAction = async () => {
+    if (!action) return;
+
+    try {
+    } catch (error) {}
+  };
+
+  const renderDialogContent = () => {
+    if (!action) return null;
+
+    const { value, label } = action;
+
+    return (
+      <DialogContent className="rounded-xl">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">
+            {label}
+          </DialogTitle>
+          {value === "rename" && (
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+
+          {["rename", "delete", "share"].includes(value) && (
+            <DialogFooter className="text-center text-light-100">
+              <Button onClick={closeAllModals}>Cancel</Button>
+              <Button onClick={handleAction}>
+                <p className="capitalize">{label}</p>
+                {isLoading && "Loading..."}
+              </Button>
+            </DialogFooter>
+          )}
+
+          {/* <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription> */}
+        </DialogHeader>
+      </DialogContent>
+    );
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
@@ -43,21 +108,28 @@ const ActionDropdown: FC<ActionDropdownProps> = ({ file }) => {
                 if (
                   ["details", "rename", "share", "delete"].includes(item.value)
                 ) {
+                  console.log("item.value: ", item.value);
                   setIsModalOpen(true);
                 }
               }}
             >
-              <Link
-                href={constructDownloadUrl(file.bucketFileId)}
-                download={file.name}
-                className="flex items-center gap-2"
-              >
-                {item.label}
-              </Link>
+              {item.value === "download" ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)}
+                  download={file.name}
+                  className="flex items-center gap-2"
+                >
+                  {item.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">{item.label}</div>
+              )}
             </DropdownMenuItem>
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
+
+      {renderDialogContent()}
     </Dialog>
   );
 };
